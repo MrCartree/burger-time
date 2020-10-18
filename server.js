febb888f31b755305565fbcbd10f3cf5deef9215
Zach LaFleur <zach.lafleur95@gmail.com>
2020-10-18 11:22:42 -0600

    created the server.js and required in all the needed stuff.

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const mysql = require("mysql");
+const routes = require("./controllers/burgers_controller");
+
+let app = express();
+
+let PORT = process.env.PORT || 8080;
+
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on : http://localhost: " + PORT);
+})
