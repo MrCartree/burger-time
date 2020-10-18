a27b66159708696d5a9a7618af20386fc515c46d
Zach LaFleur <zach.lafleur95@gmail.com>
2020-10-18 15:02:56 -0600

    added a true parameter to the seeds DB

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 USE burgers_db;
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES ("Pretzel Bacon Cheeseb", false), ("Classic Cheddar", false), ("The Works", false), ("Salve Satán", false);
+VALUES ("Pretzel Bacon Cheeseb", false), ("Classic Cheddar", false), ("The Works", false), ("Salve Satán", false), ("Un Oeuf", true);
