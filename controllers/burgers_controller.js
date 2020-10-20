358a74342f6dc90cfbac8e3a78219a420156f572
Zach LaFleur <zach.lafleur95@gmail.com>
2020-10-19 19:38:19 -0600

    Re added my up chuck function and got it to work and stuff.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -28,6 +28,21 @@ router.put("/api/burgers/:id", function(req, res) {
     });
 });
 
+router.delete("/api/burgers/:id", function(req, res) {
+    let condition = "id = " + req.params.id;
+    
+    burger.update({
+        devoured: req.body.devoured - 1
+    }, condition, function(result) {
+
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
 router.post("/api/burgers", function(req, res) {
     burger.create([
       "burger_name"
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -34,4 +34,22 @@ $(function() {
       );
     });
 
+    $(".tbPuke").on("click", function(event) {
+      var id = $(this).data("id");
+      var burger = $(this).data("burger");
+  
+      var devoured = {
+        devoured: burger
+      };
+  
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE",
+        data: devoured
+      }).then(
+        function() {
+          location.reload();
+        }
+      );
+    });
+
 });
